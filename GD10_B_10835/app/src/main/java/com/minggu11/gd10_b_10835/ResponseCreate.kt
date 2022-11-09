a2ecd4ee1e91c1aa49688bc8bfac70f75d0751a6
Giovanni040204 <111994851+Giovanni040204@users.noreply.github.com>
2022-11-09 17:25:40 +0700

    Upload file AndroidStudio

---
+++ GD10_B_10835/app/src/main/java/com/minggu11/gd10_b_10835/ResponseCreate.kt
@@ -0,0 +1,9 @@
+package com.minggu11.gd10_b_10835
+
+import com.google.gson.annotations.SerializedName
+
+class ResponseCreate (
+    @SerializedName("status") val stt:Int,
+    @SerializedName("error") val e:Boolean,
+    @SerializedName("message") val pesan:String,
+)
---
+++ GD10_B_10835/app/src/main/java/com/minggu11/gd10_b_10835/ResponseDataMahasiswa.kt
@@ -0,0 +1,8 @@
+package com.minggu11.gd10_b_10835
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseDataMahasiswa(
+    @SerializedName("status") val stt:String,
+    val totaldata: Int,
+    val data:List<MahasiswaData>)
